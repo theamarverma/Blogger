9147440a26a23ebeb3df5318f52fa2a77b54075b
Amar Verma <77249292+theamarverma@users.noreply.github.com>
2024-07-01 16:48:31 +0530

    fixed env

--- app/_lib/config/db.js
+++ app/_lib/config/db.js
@@ -1,6 +1,25 @@
 import mongoose from 'mongoose';
 
+const MONGODB_URI = process.env.MONGODB_URI;
+
+console.log('MONGODB_URI:', MONGODB_URI); // Add this line to debug
+
+if (!MONGODB_URI) {
+	throw new Error(
+		'Please define the MONGODB_URI environment variable inside .env.local'
+	);
+}
+
 export const connectDB = async () => {
-	await mongoose.connect(`${process.env.MONGO_URI}/blog-app`);
-	console.log('Db connected successfully');
+	try {
+		const status = await mongoose.connect(MONGODB_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		// console.log(status.connection.host);s
+		console.log('Db connected successfully');
+	} catch (error) {
+		console.error('Error connecting to database', error);
+		process.exit(1);
+	}
 };
